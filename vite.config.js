3557dd5f27150a64f39d71edfe0f5fd4001b1020
Ivan Porodin <MAGORKAKSL@yandex.ru>
2026-03-18 11:08:57 +0700

    Happy birthday2

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hapAlinka/', // для локальной разработки
-  // Если будете деплоить на GitHub Pages, раскомментируйте:
-  // base: '/hapAlinka/',
+  base: '/hapAlinka/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
